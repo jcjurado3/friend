1030503ad4684c1bb1be83f8c146906c82433af1
Jimmy Jurado <jurado140@gmail.com>
2023-02-23 15:41:08 -0500

    Add second array method

--- friend.rb
+++ friend.rb
@@ -3,3 +3,11 @@ friends = ["john", "jason", "jay", "Jimmy", "james"]
 p friends [3]
 p friends [4]
 p friends [0]
+
+places = ["FL", "CO", "CA", "NY", "VA"]
+
+places.pop 
+
+places.shift 
+
+p places
